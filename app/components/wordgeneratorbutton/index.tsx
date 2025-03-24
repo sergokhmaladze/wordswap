2086b55152138dfb2eb1ad6b4eef7af3c282b4bd
sergo <sergo.khmaladze@gmail.com>
2025-03-24 17:07:58 +0400

    create initial project and adding some styleing and components

---
+++ app/components/wordgeneratorbutton/index.tsx
@@ -0,0 +1,46 @@
+'use client';
+import React, { useState, useEffect } from 'react';
+import styles from './styles.module.css';
+
+interface Word {
+  english: string;
+  georgian: string;
+}
+
+function WordGenerator(): React.JSX.Element {
+  const [words, setWords] = useState<Word[]>([]);
+  const [randomWord, setRandomWord] = useState<string>('');
+
+  useEffect(() => {
+    fetch('/words.json')
+      .then((response) => response.json())
+      .then((data: Word[]) => setWords(data))
+      .catch((error) => console.error('error loading json:', error));
+  }, []);
+
+  const generateRandomWord = () => {
+    if (words.length > 0) {
+      const randomIndex = Math.floor(Math.random() * words.length);
+      setRandomWord(words[randomIndex].georgian);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <button
+          onClick={generateRandomWord}
+          type="button"
+          className={styles.container__button}
+        >
+          Generate Word
+        </button>
+      </div>
+      <div className={styles.container__randomWord}>
+        <p className={styles.container__randomWord__dis}>{randomWord}</p>
+      </div>
+    </>
+  );
+}
+
+export default WordGenerator;
